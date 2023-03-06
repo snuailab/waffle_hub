79bf0e01dd49c16a783ffb6f6a38d0848b848948
ZeroAct <huijae.lee@snuailab.ai>
2023-03-06 10:57:37 +0900

    ultralytics training

--- waffle_hub/hub/base_hub.py
+++ waffle_hub/hub/base_hub.py
@@ -3,6 +3,7 @@ from abc import abstractmethod
 from functools import cached_property
 from pathlib import Path
 
+from waffle_utils.file import io
 from waffle_utils.utils import type_validator
 
 logger = logging.getLogger(__name__)
@@ -10,54 +11,111 @@ logger = logging.getLogger(__name__)
 
 class BaseHub:
 
+    AVAILABLE_TASK = []
+    AVAILABLE_MODEL = []
+    AVAILABLE_SIZE = []
+
     # directory settings
     DEFAULT_ROOT_DIR = Path("./models")
 
     TRAIN_DIR = Path("trains")
+    RAW_TRAIN_DIR = Path("raw_train")
+
     INFERENCE_DIR = Path("inferences")
     EVALUATION_DIR = Path("evaluations")
     EXPORT_DIR = Path("exports")
 
     # common files
-    LAST_CKPT_FILE = TRAIN_DIR / "weights/last_ckpt.pth"
-    BEST_CKPT_FILE = TRAIN_DIR / "weights/best_ckpt.pth"
+    TRAIN_OPTION_FILE = "configs/common.yaml"
+    LAST_CKPT_FILE = "weights/last_ckpt.pth"
+    BEST_CKPT_FILE = "weights/best_ckpt.pth"
+    METRIC_FILE = "metrics.txt"
 
-    # train files
-    TRAIN_LOG_FILE = TRAIN_DIR / "log.txt"
-    METRIC_FILE = TRAIN_DIR / "metrics.txt"
+    def __init__(
+        self,
+        name: str,
+        task: str = None,
+        model_name: str = None,
+        model_size: str = None,
+        pretrained_model: str = None,
+        root_dir: str = None,
+    ):
 
-    # infer files
-    INFERENCE_LOG_FILE = INFERENCE_DIR / "log.txt"
+        self.name = name
+        self.task = task
+        self.model_name = model_name
+        self.model_size = model_size
+        self.pretrained_model = pretrained_model
+        self.root_dir = root_dir
 
-    # eval files
-    EVALUATION_LOG_FILE = EVALUATION_DIR / "log.txt"
-
-    # export files
-    EXPORT_LOG_FILE = EXPORT_DIR / "log.txt"
-
-    def __init__(self, name: str, root_dir: str = None):
-
-        self.name = self._name = name
-        self.root_dir = self._root_dir = root_dir
+        self.validate_model()
 
     # properties
     @property
     def name(self):
-        return self._name
+        return self.__name
 
     @name.setter
     @type_validator(str)
     def name(self, v):
-        self._name = v
+        self.__name = v
 
     @property
     def root_dir(self) -> Path:
-        return self._root_dir
+        return self.__root_dir
 
     @root_dir.setter
-    @type_validator(str)
+    @type_validator(Path, strict=False)
     def root_dir(self, v):
-        self._root_dir = Path(v) if v else BaseHub.DEFAULT_ROOT_DIR
+        self.__root_dir = Path(v) if v else BaseHub.DEFAULT_ROOT_DIR
+
+    @property
+    def task(self) -> str:
+        return self.__task
+
+    @task.setter
+    @type_validator(str)
+    def task(self, v):
+        if v not in self.AVAILABLE_TASK:
+            raise ValueError(
+                f"Task {v} is not supported. Choose one of {self.AVAILABLE_TASK}"
+            )
+        self.__task = v
+
+    @property
+    def model_name(self) -> str:
+        return self.__model_name
+
+    @model_name.setter
+    @type_validator(str)
+    def model_name(self, v):
+        if v not in self.AVAILABLE_MODEL:
+            raise ValueError(
+                f"Model {v} is not supported. Choose one of {self.AVAILABLE_MODEL}"
+            )
+        self.__model_name = v
+
+    @property
+    def model_size(self) -> str:
+        return self.__model_size
+
+    @model_size.setter
+    @type_validator(str)
+    def model_size(self, v):
+        if v not in self.AVAILABLE_SIZE:
+            raise ValueError(
+                f"Model Size {v} is not supported. Choose one of {self.AVAILABLE_SIZE}"
+            )
+        self.__model_size = v
+
+    @property
+    def pretrained_model(self) -> str:
+        return self.__pretrained_model
+
+    @pretrained_model.setter
+    @type_validator(str)
+    def pretrained_model(self, v):
+        self.__pretrained_model = v
 
     @cached_property
     def model_dir(self) -> Path:
@@ -66,6 +124,10 @@ class BaseHub:
     @cached_property
     def train_dir(self) -> Path:
         return self.model_dir / BaseHub.TRAIN_DIR
+
+    @cached_property
+    def raw_train_dir(self) -> Path:
+        return self.model_dir / BaseHub.RAW_TRAIN_DIR
 
     @cached_property
     def inference_dir(self) -> Path:
@@ -78,6 +140,39 @@ class BaseHub:
     @cached_property
     def export_dir(self) -> Path:
         return self.model_dir / BaseHub.EXPORT_DIR
+
+    @cached_property
+    def train_option_file(self) -> Path:
+        return self.train_dir / BaseHub.TRAIN_OPTION_FILE
+
+    @cached_property
+    def best_ckpt_file(self) -> Path:
+        return self.train_dir / BaseHub.BEST_CKPT_FILE
+
+    @cached_property
+    def last_ckpt_file(self) -> Path:
+        return self.train_dir / BaseHub.LAST_CKPT_FILE
+
+    @cached_property
+    def metric_file(self) -> Path:
+        return self.train_dir / BaseHub.METRIC_FILE
+
+    def delete_train(self):
+        """Delete Raw Trained Data. It can be trained again."""
+        io.remove_directory(self.raw_train_dir)
+
+    def is_trainable(self):
+        if self.raw_train_dir.exists():
+            raise FileExistsError(
+                f"""
+                Train[{self.name}] already exists.
+                Use another name or delete trains (hub.delete_train()).
+                """
+            )
+
+    @abstractmethod
+    def validate_model(self):
+        raise NotImplementedError
 
     @abstractmethod
     def train(self):
--- waffle_hub/hub/ultralytics_hub.py
+++ waffle_hub/hub/ultralytics_hub.py
@@ -22,15 +22,157 @@ except ModuleNotFoundError as e:
     raise e
 
 
+from pathlib import Path
+
+from pydantic import BaseModel
+from ultralytics import YOLO
+from waffle_utils.file import io
+
 from . import BaseHub
+
+
+class BaseModelParams(BaseModel):
+    model_name: str
+    image_size: str
 
 
 class UltralyticsHub(BaseHub):
 
-    AVAILABLE_TASK = ["detection", "classification", "segmentation"]
+    # Common
+    AVAILABLE_TASK = ["detect", "classify", "segment"]
+    AVAILABLE_MODEL = ["yolov8"]
+    AVAILABLE_SIZE = list("nsmlx")
 
-    def train(self):
-        raise NotImplementedError
+    # Backend Specifics
+    TASK_SUFFIX = {
+        "detect": "",
+        "classify": "-cls",
+        "segment": "-seg",
+    }
+
+    def __init__(
+        self,
+        name: str,
+        task: str = None,
+        model_name: str = None,
+        model_size: str = None,
+        pretrained_model: str = None,
+        root_dir: str = None,
+    ):
+
+        super().__init__(
+            name=name,
+            task=task,
+            model_name=model_name,
+            model_size=model_size,
+            pretrained_model=pretrained_model,
+            root_dir=root_dir,
+        )
+
+        # ultralytics specific variables
+        self._model = (
+            self.model_name
+            + self.model_size
+            + self.TASK_SUFFIX[self.task]
+            + ".pt"
+            if pretrained_model is None
+            else pretrained_model
+        )
+
+    def validate_model(self):
+        return
+
+    def train(
+        self,
+        dataset_dir: str,
+        epochs: int,
+        batch_size: int,
+        image_size: int,
+        device: str = "0",
+        workers: int = 2,
+        seed: int = 0,
+        verbose: bool = True,
+    ):
+        self.is_trainable()
+
+        # set data
+        dataset_dir: Path = Path(dataset_dir)
+        if self.task in ["detect", "segment"]:
+            if dataset_dir.suffix not in [".yml", ".yaml"]:
+                yaml_files = list(dataset_dir.glob("*.yaml")) + list(
+                    dataset_dir.glob("*.yml")
+                )
+                if len(yaml_files) != 1:
+                    raise FileNotFoundError(
+                        f"Ambiguous data file. Detected files: {yaml_files}"
+                    )
+                data = Path(yaml_files[0]).absolute()
+            else:
+                data = dataset_dir.absolute()
+        elif self.task == "classify":
+            if not dataset_dir.is_dir():
+                raise ValueError(
+                    f"Classification dataset should be directory. Not {dataset_dir}"
+                )
+            data = dataset_dir.absolute()
+        data = str(data)
+
+        # save train configs. TODO: Generalize using pydantic
+        io.save_yaml(
+            {
+                "task": self.task,
+                "model_name": self.model_name,
+                "model_size": self.model_size,
+                "pretrained_model": self.pretrained_model
+                if self.pretrained_model
+                else self._model,
+                "image_size": image_size,
+                "epochs": epochs,
+                "batch_size": batch_size,
+                "seed": seed,
+            },
+            self.train_option_file,
+            create_directory=True,
+        )
+
+        try:
+            model = YOLO(self._model, task=self.task)
+            model.train(
+                data=data,
+                epochs=epochs,
+                batch=batch_size,
+                imgsz=image_size,
+                device=device,
+                workers=workers,
+                seed=seed,
+                verbose=verbose,
+                project=self.model_dir,
+                name=self.RAW_TRAIN_DIR,
+            )
+
+            # Parse Training Results
+            io.copy_file(
+                self.raw_train_dir / "weights" / "best.pt",
+                self.best_ckpt_file,
+                create_directory=True,
+            )
+            io.copy_file(
+                self.raw_train_dir / "weights" / "last.pt",
+                self.last_ckpt_file,
+                create_directory=True,
+            )
+            io.copy_file(
+                self.raw_train_dir / "results.csv",
+                self.metric_file,
+                create_directory=True,
+            )
+
+            return str(self.train_dir)
+
+        except Exception as e:
+            if self.raw_train_dir.exists():
+                io.remove_directory(self.raw_train_dir)
+            raise e
 
     def inference(self):
         raise NotImplementedError
