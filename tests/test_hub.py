8f375943da793b391634478fa7b068f398afe699
ZeroAct <huijae.lee@snuailab.ai>
2023-04-03 18:21:17 +0900

    Update test code

--- tests/test_hub.py
+++ tests/test_hub.py
@@ -1,3 +1,4 @@
+import time
 from pathlib import Path
 
 import pytest
@@ -60,7 +61,7 @@ def test_ultralytics_object_detection(tmpdir: Path, dummy_dataset: Dataset):
         device="cpu",
     )
     assert train_callback.get_progress() == 1
-    assert len(train_callback.get_result()) == 1
+    assert len(train_callback.get_metrics()) == 1
     assert Path(train_callback.best_ckpt_file).exists()
     assert Path(train_callback.last_ckpt_file).exists()
     assert Path(train_callback.metric_file).exists()
@@ -106,7 +107,7 @@ def test_ultralytics_classification(tmpdir: Path, dummy_dataset: Dataset):
         device="cpu",
     )
     assert train_callback.get_progress() == 1
-    assert len(train_callback.get_result()) == 1
+    assert len(train_callback.get_metrics()) == 1
     assert Path(train_callback.best_ckpt_file).exists()
     assert Path(train_callback.last_ckpt_file).exists()
     assert Path(train_callback.metric_file).exists()
@@ -120,4 +121,61 @@ def test_ultralytics_classification(tmpdir: Path, dummy_dataset: Dataset):
     assert Path(inference_callback.inference_dir).exists()
 
     export_callback: ExportCallback = hub.export()
+    assert Path(export_callback.export_file).exists()
+
+
+def test_non_hold(tmpdir: Path, dummy_dataset: Dataset):
+
+    export_dir = dummy_dataset.export("yolo_detection")
+
+    name = "test_det"
+
+    hub = UltralyticsHub(
+        name=name,
+        task="object_detection",
+        model_type="yolov8",
+        model_size="n",
+        classes=["1", "2"],
+        root_dir=tmpdir,
+    )
+    hub = UltralyticsHub.load(name=name, root_dir=tmpdir)
+    hub: UltralyticsHub = UltralyticsHub.from_model_config(
+        name=name,
+        model_config_file=tmpdir / name / UltralyticsHub.MODEL_CONFIG_FILE,
+        root_dir=tmpdir,
+    )
+    train_callback: TrainCallback = hub.train(
+        dataset_path=export_dir,
+        epochs=1,
+        batch_size=4,
+        image_size=32,
+        pretrained_model=None,
+        device="cpu",
+        hold=False,
+    )
+
+    while not train_callback.is_finished():
+        time.sleep(0.1)
+
+    assert train_callback.get_progress() == 1
+    assert len(train_callback.get_metrics()) == 1
+    assert Path(train_callback.best_ckpt_file).exists()
+    assert Path(train_callback.last_ckpt_file).exists()
+    assert Path(train_callback.metric_file).exists()
+    assert Path(train_callback.result_dir).exists()
+
+    inference_callback: InferenceCallback = hub.inference(
+        source=export_dir, device="cpu", hold=False
+    )
+    while not inference_callback.is_finished():
+        time.sleep(0.1)
+
+    assert inference_callback.get_progress() == 1
+    assert Path(inference_callback.inference_dir).exists()
+
+    export_callback: ExportCallback = hub.export(hold=False)
+
+    while not export_callback.is_finished():
+        time.sleep(0.1)
+
     assert Path(export_callback.export_file).exists()
