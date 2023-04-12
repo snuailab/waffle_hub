0c43ced5c82cea507cde8c8549d827aae94f191a
cys <gkwns9006@snuailab.ai>
2023-04-12 15:49:05 +0900

    add inference

--- waffle_hub/hub/adapter/hugging_face/hugging_face_hub.py
+++ waffle_hub/hub/adapter/hugging_face/hugging_face_hub.py
@@ -10,23 +10,32 @@ BACKEND_VERSION = get_installed_backend_version(BACKEND_NAME)
 import os
 import warnings
 from dataclasses import dataclass, field
+from pathlib import Path
 from typing import Callable, Union
 
 import albumentations
+import cv2
 import numpy as np
+import torch
+import tqdm
 from torchvision import transforms as T
 from transformers import (
     AutoImageProcessor,
     AutoModelForObjectDetection,
     Trainer,
+    TrainerCallback,
     TrainingArguments,
     pipeline,
 )
+from waffle_utils.file import io
 
 from datasets import DatasetDict, load_from_disk
 from waffle_hub.hub.base_hub import BaseHub
-from waffle_hub.schema.configs import TrainConfig
-from waffle_hub.utils.callback import TrainCallback
+from waffle_hub.hub.model.wrapper import ModelWrapper, get_parser
+from waffle_hub.schema.configs import InferenceConfig, TrainConfig
+from waffle_hub.utils.callback import InferenceCallback, TrainCallback
+from waffle_hub.utils.data import ImageDataset
+from waffle_hub.utils.draw import draw_results
 
 
 @dataclass
@@ -43,8 +52,34 @@ class HuggingFaceHub(BaseHub):
     # Common
     MODEL_TYPES = {
         "object_detection": {
-            "DETA": ["base", "large"],
-            "DETR": ["base", "large"],
+            "DETA": {
+                "base": "jozhang97/deta-resnet-50",
+            },
+            "DETR": {
+                "base": "facebook/detr-resnet-50",
+                "large": "facebook/detr-resnet-101",
+            },
+            "YOLO": {
+                "base": "mhyatt000/YOLOv5",
+                "large": "kadirnar/yolov8l-v8.0",
+            },
+        },
+    }
+
+    DEFAULT_PARAMAS = {
+        "object_detection": {
+            "epochs": 50,
+            "image_size": [640, 640],
+            "learning_rate": 5e-05,
+            "letter_box": True,  # TODO: implement letter_box
+            "batch_size": 16,
+        },
+        "classification": {
+            "epochs": 50,
+            "image_size": [224, 224],
+            "learning_rate": 5e-05,
+            "letter_box": False,
+            "batch_size": 16,
         },
     }
 
@@ -82,7 +117,11 @@ class HuggingFaceHub(BaseHub):
             root_dir=root_dir,
         )
 
+        self.pretrained_model: str = self.MODEL_TYPES[self.task][
+            self.model_type
+        ][self.model_size]
         self.train_input = TrainInput()
+        self.best_ckpt_dir = self.hub_dir / "weights" / "best_ckpt"
 
     @classmethod
     def new(
@@ -104,16 +143,14 @@ class HuggingFaceHub(BaseHub):
             root_dir=root_dir,
         )
 
-    def get_metrics(self) -> list[list[dict]]:
-        # TODO: implement
-        return []
-
     def on_train_start(self, cfg: TrainConfig):
-        # TODO: model choice
+        # overwrite train config with default config
+        cfg.pretrained_model = self.pretrained_model
+        for k, v in cfg.to_dict().items():
+            if v is None:
+                setattr(cfg, k, self.DEFAULT_PARAMAS[self.task][k])
 
         os.environ["CUDA_VISIBLE_DEVICES"] = cfg.device
-
-        cfg.pretrained_model = f"facebook/{self.model_type.lower()}-resnet-50"
 
         dataset = load_from_disk(cfg.dataset_path)
 
@@ -183,6 +220,7 @@ class HuggingFaceHub(BaseHub):
             )
 
         dataset["train"] = dataset["train"].with_transform(transform_aug_ann)
+        dataset["val"] = dataset["val"].with_transform(transform_aug_ann)
 
         self.train_input.train_data = dataset
 
@@ -216,23 +254,129 @@ class HuggingFaceHub(BaseHub):
             num_train_epochs=cfg.epochs,
             remove_unused_columns=False,
             push_to_hub=False,
+            logging_strategy="epoch" if cfg.verbose else "no",
+            evaluation_strategy="epoch",
+            learning_rate=cfg.learning_rate,
+            dataloader_num_workers=cfg.workers,
+            seed=cfg.seed,
+            greater_is_better=True,
         )
 
     def training(self, cfg: TrainConfig, callback: TrainCallback):
+
         trainer = Trainer(
             model=self.train_input.model,
             args=self.train_input.training_args,
             data_collator=self.train_input.collate_fn,
             train_dataset=self.train_input.train_data["train"],
+            eval_dataset=self.train_input.train_data["val"],
             tokenizer=self.train_input.image_processor,
         )
         trainer.train()
         trainer.save_model(str(self.artifact_dir / "weights"))
+        self.train_log = trainer.state.log_history
+
+    def get_metrics(self) -> list[list[dict]]:
+        metrics = []
+
+        for epoch in range(
+            0, len(self.train_log) - 1, 2
+        ):  # last is runtime info
+            train_log, eval_log = (
+                self.train_log[epoch],
+                self.train_log[epoch + 1],
+            )
+            epoch_metrics = []
+
+            train_log.update(eval_log)
+            for key, value in train_log.items():
+                epoch_metrics.append({"tag": key, "value": value})
+            metrics.append(epoch_metrics)
+
+        return metrics
 
     def on_train_end(self, cfg: TrainConfig):
-        return super().on_train_end(cfg)
+        io.copy_files_to_directory(
+            self.artifact_dir / "weights",
+            self.best_ckpt_dir,
+            create_directory=True,
+        )
+        io.save_json(self.get_metrics(), self.metric_file)
 
-    # Inference Hook
+    def get_preprocess(self, task, pretrained_model: str):
+        image_processer = AutoImageProcessor.from_pretrained(pretrained_model)
+
+        normalize = T.Normalize(
+            image_processer.image_mean, image_processer.image_std, inplace=True
+        )
+
+        def preprocess(x, *args, **kwargs):
+            return normalize(x)
+
+        return preprocess
+
+    def get_postprocess(self, task):
+        if task == "classification":
+
+            def inner(x: torch.Tensor, *args, **kwargs):
+                return [x]
+
+        elif task == "object_detection":
+
+            def inner(x: torch.Tensor, *args, **kwargs):
+
+                confidences, category_ids = torch.max(
+                    x.logits[:, :, :-1], dim=-1
+                )
+                cxcywh = x.pred_boxes[:, :, :4]
+                cx, cy, w, h = torch.unbind(cxcywh, dim=-1)
+                x1 = cx - w / 2
+                y1 = cy - h / 2
+                x2 = cx + w / 2
+                y2 = cy + h / 2
+                xyxy = torch.stack([x1, y1, x2, y2], dim=-1)
+
+                return xyxy, confidences, category_ids
+
+        else:
+            raise NotImplementedError(f"Task {task} is not implemented.")
+
+        return inner
+
     def get_model(self):
-        model = pipeline(model="")  # ????
+        """Get model.
+        Returns:
+            ModelWrapper: Model wrapper
+        """
+        self.check_train_sanity()
+
+        # get adapt functions
+        preprocess = self.get_preprocess(self.task, self.pretrained_model)
+        postprocess = self.get_postprocess(self.task)
+
+        # get model
+        model = AutoModelForObjectDetection.from_pretrained(
+            str(self.best_ckpt_dir),
+        )
+
+        model = ModelWrapper(
+            model=model.eval(),
+            preprocess=preprocess,
+            postprocess=postprocess,
+        )
+
         return model
+
+    def check_train_sanity(self) -> bool:
+        """Check if all essential files are exist.
+
+        Returns:
+            bool: True if all files are exist else False
+        """
+        if not (
+            self.model_config_file.exists()
+            and self.best_ckpt_dir.exists()
+            # and self.last_ckpt_file.exists()
+        ):
+            raise FileNotFoundError("Train first! hub.train(...).")
+        return True
