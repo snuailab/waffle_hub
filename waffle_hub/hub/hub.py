db2471d4f10a920c071cfd2cc3c4e7fa367f5cb6
cys <gkwns9006@snuailab.ai>
2023-07-04 14:20:46 +0900

    fix to remove arifact

--- waffle_hub/hub/hub.py
+++ waffle_hub/hub/hub.py
@@ -911,6 +911,8 @@ class Hub:
                 )
                 self.after_train(cfg, result)
                 callback.force_finish()
+            except FileExistsError as e:
+                raise e
             except Exception as e:
                 if self.artifact_dir.exists():
                     io.remove_directory(self.artifact_dir)
