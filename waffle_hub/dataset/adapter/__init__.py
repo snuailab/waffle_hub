41939eec27b95a631fb382dbd97f1c3c5c289a6e
ZeroAct <lhj5682@naver.com>
2023-08-16 14:16:57 +0900

    add label studio import

--- waffle_hub/dataset/adapter/__init__.py
+++ waffle_hub/dataset/adapter/__init__.py
@@ -1,5 +1,6 @@
 from .autocare_dlt import export_autocare_dlt, import_autocare_dlt
 from .coco import export_coco, import_coco
+from .label_studio import import_label_studio
 from .transformers import export_transformers, import_transformers
 from .yolo import export_yolo, import_yolo
 
@@ -12,4 +13,5 @@ __all__ = [
     "import_coco",
     "import_transformers",
     "import_yolo",
+    "import_label_studio",
 ]
