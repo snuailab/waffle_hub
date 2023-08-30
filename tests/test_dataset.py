21af93d7d43dc1ed1c07f9b620c3d0c18a56f46e
ljj727 <kaejong2@gmail.com>
2023-08-30 16:15:53 +0900

    Add Superb AI TEST

--- tests/test_dataset.py
+++ tests/test_dataset.py
@@ -255,6 +255,16 @@ def _export(dataset_name, task: TaskType, root_dir):
             root_dir=root_dir,
         )
         assert len(dataset.get_images()) == len(import_ds.get_images())
+    if task in [TaskType.OBJECT_DETECTION]:
+        export_dir = Path(dataset.export("superb_ai"))
+        import_ds = Dataset.from_superb_ai(
+            name=f"{task}_import_superb_ai",
+            task=task,
+            superb_root_dir = export_dir,
+            superb_file_dir = export_dir,
+            root_dir=root_dir,
+        )
+        assert len(dataset.get_images()) == len(import_ds.get_images())
 
 
 # test dummy
