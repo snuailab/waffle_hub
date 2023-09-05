4ed415efe469d77136fe72896df33c5994271784
Jiyong-Jeon <jiyong@snuailab.ai>
2023-09-05 17:11:55 +0900

    Update pytest utils

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,9 +1,15 @@
+from itertools import permutations
+
+import numpy as np
+import pytest
+
 from waffle_hub.schema.evaluate import (
     ClassificationMetric,
     InstanceSegmentationMetric,
     ObjectDetectionMetric,
 )
 from waffle_hub.schema.fields import Annotation
+from waffle_hub.utils.data import resize_image
 from waffle_hub.utils.evaluate import (
     evaluate_classification,
     evaluate_object_detection,
@@ -69,3 +75,69 @@ def test_evaluate_object_detection():
     )
 
     assert result.mAP < 1.0
+
+
+def test_resize_function():
+    letter_box = [False, True]
+    image_size = [224, 480, 512, 640, 1280, 1600, 1920]
+
+    rect_list = permutations(image_size, 2)
+    square_list = [(sz, sz) for sz in image_size]
+
+    for lb in letter_box:
+        # square 2 square
+        for ori_shape in square_list:
+            for resize_shape in square_list:
+                image, info = resize_image(
+                    np.random.randint(0, 255, ori_shape + (3,), dtype=np.uint8),
+                    resize_shape,
+                    letter_box=lb,
+                )
+                assert (image.shape[1], image.shape[0]) == info[
+                    "new_shape"
+                ], f"Difference from resize image shape to info (letter_box: {lb})"
+                assert (
+                    info["input_shape"] == info["new_shape"]
+                ), "Difference from info[input_shape] to info[new_shape] (letter_box: {lb})"
+        # square 2 rect
+        for ori_shape in square_list:
+            for resize_shape in rect_list:
+                image, info = resize_image(
+                    np.random.randint(0, 255, ori_shape + (3,), dtype=np.uint8),
+                    resize_shape,
+                    letter_box=lb,
+                )
+                assert (image.shape[1], image.shape[0]) == info[
+                    "new_shape"
+                ], "Difference from resize image shape to info (letter_box: {lb})"
+                assert (
+                    info["input_shape"] == info["new_shape"]
+                ), "Difference from info[input_shape] to info[new_shape] (letter_box: {lb})"
+        # rect 2 square
+        for ori_shape in rect_list:
+            for resize_shape in square_list:
+                image, info = resize_image(
+                    np.random.randint(0, 255, ori_shape + (3,), dtype=np.uint8),
+                    resize_shape,
+                    letter_box=lb,
+                )
+                assert (image.shape[1], image.shape[0]) == info[
+                    "new_shape"
+                ], "Difference from resize image shape to info (letter_box: {lb})"
+                assert (
+                    info["input_shape"] == info["new_shape"]
+                ), "Difference from info[input_shape] to info[new_shape] (letter_box: {lb})"
+        # rect 2 rect
+        for ori_shape in rect_list:
+            for resize_shape in rect_list:
+                image, info = resize_image(
+                    np.random.randint(0, 255, ori_shape + (3,), dtype=np.uint8),
+                    resize_shape,
+                    letter_box=lb,
+                )
+                assert (image.shape[1], image.shape[0]) == info[
+                    "new_shape"
+                ], "Difference from resize image shape to info (letter_box: {lb})"
+                assert (
+                    info["input_shape"] == info["new_shape"]
+                ), "Difference from info[input_shape] to info[new_shape] (letter_box: {lb})"
