f8696a0efbc72e92e5e2df2ef1280ad65271aaaf
kimdanni <hyperreal_s2@naver.com>
2023-09-15 18:34:59 +0900

    hpo optuna

--- waffle_hub/core/hpo/framework/optuna_hpo.py
+++ waffle_hub/core/hpo/framework/optuna_hpo.py
@@ -4,8 +4,13 @@ from pathlib import Path
 import optuna
 import optuna.visualization as oplt
 import plotly.io as pio
+from optuna.pruners import HyperbandPruner
+from optuna.samplers import GridSampler, RandomSampler, TPESampler
 
 from waffle_hub.schema.configs import OptunaHpoMethodConfig
+
+# TODO [Exception]: scheduler 및 pruner 관련 none 일 경우 (config 에서도 error 가 발생할 수 있음) 하위 계층에서도 error 발생
+#      class HPOMethodError(Exception) 정의 필요
 
 
 class OptunaHPO:
@@ -56,13 +61,14 @@ class OptunaHPO:
         # db must be located in study hub
 
     def visualize_hpo_results(self):
-
+        # 시각화 생성
         param_importance = oplt.plot_param_importances(self._study)
         contour = oplt.plot_contour(self._study)
         coordinates = oplt.plot_parallel_coordinate(self._study)
         slice_plot = oplt.plot_slice(self._study)
         optimization_history = oplt.plot_optimization_history(self._study)
 
+        # 생성한 시각화를 이미지 파일로 저장
         pio.write_image(param_importance, self._hub_root / self._study_name / "param_importance.png")
         pio.write_image(contour, self._hub_root / self._study_name / "contour.png")
         pio.write_image(coordinates, self._hub_root / self._study_name / "coordinates.png")
@@ -74,12 +80,12 @@ class OptunaHPO:
     def optimize(self, objective, dataset, n_trials, search_space, **kwargs):
         def objective_wrapper(trial):
             def _get_search_space(trial, search_space):
-                params = {}
-                for k, v in search_space.items():
-                    if isinstance(v, tuple):
-                        params[k] = trial.suggest_categorical(k, v)
-                    else:
-                        params[k] = trial.suggest_float(k, v[0], v[1])
+                params = {
+                    k: trial.suggest_categorical(k, v)
+                    if isinstance(v, tuple)
+                    else trial.suggest_float(k, v[0], v[1])
+                    for k, v in search_space.items()
+                }
                 return params
 
             params = _get_search_space(trial, search_space)
